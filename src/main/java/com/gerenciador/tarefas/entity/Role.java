98a0dd26fce817e0d167324cb6498cd47625b762
Pammyla Gonzaga <pammyla.gonzaga@gmail.com>
2026-01-12 18:38:43 -0300

    Aula23 Criando API de cadastro de usuário

---
+++ src/main/java/com/gerenciador/tarefas/entity/Role.java
@@ -0,0 +1,27 @@
+package com.gerenciador.tarefas.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Getter
+@Setter
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String nome;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<Usuario> usuarios;
+}
